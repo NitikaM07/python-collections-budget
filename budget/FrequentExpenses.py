fa3f85a4a04eeec7e974be39af7bc27758da655c
NitikaM07 <42680197+NitikaM07@users.noreply.github.com>
2023-03-29 14:58:52 +0530

    Add files via upload

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,16 +1,16 @@
 from . import Expense
 import collections
-import matlplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 expenses =Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 spending_counter = collections.Counter(spending_categories)
-#print(spending_counter)
-top5=spending_counter.most_common(5)
-categories,count=zip(*top5)
-fig,ax=plt.subplots()
+#print(spending_counter) to check counter
+top5= spending_counter.most_common(5)
+categories, count = zip(*top5)
+fig, ax = plt.subplots()
 ax.bar(categories,count)
-ax.set_title('# of Pruchases by Category')
+ax.set_title('# of Purchases by Category')
 plt.show()
